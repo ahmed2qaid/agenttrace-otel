6eca9ceb6002537047cb5d035b3611487d00fd2b
AHMED QAID <ahmed0qaid@gmail.com>
2026-08-30 17:49:48 +0300

    Export v0.3 replay and regression APIs

--- src/agenttrace_otel/__init__.py
+++ src/agenttrace_otel/__init__.py
@@ -1,20 +1,32 @@
+from .analysis import GatePolicy, GateResult, GateViolation, RunMetrics, evaluate_gate, run_metrics
 from .core import AgentTraceSession, TraceEvent, Trajectory, TrajectoryDiff, diff_trajectories
 from .langgraph import LangGraphEventAdapter
 from .mcp import TracedMCPClient
 from .n8n import import_n8n_execution
 from .openai_agents import OpenAIAgentsTraceHooks
+from .replay import load_replay, save_replay, trajectory_from_replay, trajectory_to_replay
 from .usage import UsageSnapshot, usage_snapshot
 
 __all__ = [
     "AgentTraceSession",
+    "GatePolicy",
+    "GateResult",
+    "GateViolation",
     "LangGraphEventAdapter",
     "OpenAIAgentsTraceHooks",
+    "RunMetrics",
     "TraceEvent",
     "TracedMCPClient",
     "Trajectory",
     "TrajectoryDiff",
     "UsageSnapshot",
     "diff_trajectories",
+    "evaluate_gate",
     "import_n8n_execution",
+    "load_replay",
+    "run_metrics",
+    "save_replay",
+    "trajectory_from_replay",
+    "trajectory_to_replay",
     "usage_snapshot",
 ]
